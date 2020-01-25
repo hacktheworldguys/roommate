ce3422e7e9391a3bc9381d6a709a2d177ac8844c
EXT02D18579 <metin.kalinsaz@consultant.turkcell.com.tr>
2020-01-25 20:26:59 +0300

    created project structure and added first simple controller

--- src/main/java/com/project/roommate/RoommateApplication.java
+++ src/main/java/com/project/roommate/RoommateApplication.java
@@ -1,4 +1,13 @@
 package com.project.roommate;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RoommateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoommateApplication.class, args);
+    }
+
 }
--- src/main/java/com/project/roommate/controller/RoommateRestController.java
+++ src/main/java/com/project/roommate/controller/RoommateRestController.java
@@ -1,4 +1,16 @@
 package com.project.roommate.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class RoommateRestController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello World!";
+    }
+
 }
